45cf202bdcac3a6a8189e16fa8193e5456720dee
alessandro-cassiani <250693256+alessandro-cassiani@users.noreply.github.com>
2025-12-24 20:09:35 +0100

    Serial write to arduino test

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -4,9 +4,13 @@ void setup() {
 	Serial.begin(115200);
 	while (!Serial) {}
 
+	Serial.setTimeout(10); // 10 milliseconds should work
 }
 
 void loop() {
-	Serial.println("Hello from arduino!");
-	delay(1000);
+	if (Serial.available() > 0) { // func return number of bytes in the buffer
+		// extremely important to use single quotes for the newline char
+		String msg = Serial.readStringUntil('\n');
+		// Check the serial LED to see if the message has arrived
+	}
 }
---
+++ raspberry-pi/scripts/serial_write_to_arduino.py
@@ -0,0 +1,21 @@
+import serial
+import time
+
+ser = serial.Serial("/dev/ttyACM0", 115200, timeout=1.0)
+
+DELAY_SERIAL_SETUP = 3
+time.sleep(DELAY_SERIAL_SETUP)
+
+ser.reset_input_buffer()
+print("Serial OK") 
+
+MESSAGE_WAIT_SEND = 1
+try:
+    while True:
+        time.sleep(MESSAGE_WAIT_SEND)
+        ser.write("Hello from raspberry pi\n".encode("utf-8")) # \n is important for message deliming
+        print("Sent message to arduino")
+        
+except KeyboardInterrupt: # Close the serial communication with ctrl + C
+    print("Close serial communication.")
+    ser.close()
