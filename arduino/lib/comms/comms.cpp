664e806cea1c1a5439b30ed47be6fae619d5984e
alessandro-cassiani <250693256+alessandro-cassiani@users.noreply.github.com>
2025-12-26 17:22:34 +0100

    No class c++ read and send functions, simple first version

--- arduino/lib/comms/comms.cpp
+++ arduino/lib/comms/comms.cpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <Arduino.h>
-//#include <PacketSerial.h>
+#include <PacketSerial.h>
 
 #include "comms.h"
 
@@ -12,8 +12,7 @@
 const uint16_t CRC_16_CCITT_POLYNOMIAL = 0x1021;
 const uint16_t CRC_16_CCITT_POLYNOMIAL_REVERSED = 0x8408;
 const uint8_t BYTE_SIZE = 8;
-
-//PacketSerial packetSerial;
+const uint8_t MAX_BUFFER_LENGTH = 255;
 
 uint16_t crc16_msb(const uint8_t *data, uint8_t length) {
     uint16_t rem = 0xFFFF; 
@@ -53,6 +52,39 @@ uint16_t crc16_lsb(const uint8_t *data, uint8_t length) {
     return rem;
 }
 
+void onPacketReceived(const uint8_t *buffer, size_t length) {
+    if (length < 4) return;
+
+    uint8_t cmd = buffer[0];
+    uint8_t len_payload = buffer[1];
+
+    if (len_payload + 4 != length) return;
+
+    uint16_t crcReceived = (buffer[length - 2] << 8  | buffer[length - 1]) & 0xFFFF;
+    uint16_t crcRecalculated = crc16_lsb(buffer, length - 2);
+
+    if (crcRecalculated != crcReceived) return; // crc error
+}
+
+PacketSerial packetSerial; // Defaults to using cobs
+
+void sendPacket(const uint8_t cmd, uint8_t length, const uint8_t *payload) {
+    if (length - 4 > MAX_BUFFER_LENGTH) return;
+
+    uint8_t buffer[MAX_BUFFER_LENGTH];
+
+    buffer[0] = cmd;
+    buffer[1] = length;
+
+    memcpy(&buffer[2], payload, length);
+
+    uint16_t crc = crc16_lsb(buffer, length + 2);
+    buffer[length + 2] = crc << 8;
+    buffer[length + 3] = crc & 0xFF;
+
+    packetSerial.send(buffer, length + 4);
+}
+
 #ifdef DEBUG
 #include <stdio.h>
 void debug_crc() {
@@ -65,6 +97,7 @@ void debug_crc() {
 }
 #endif
 
+#ifndef Arduino_h
 int main() {
     #ifdef DEBUG
     debug_crc();
@@ -72,5 +105,6 @@ int main() {
 
     return 0;
 }
+#endif
 
 
